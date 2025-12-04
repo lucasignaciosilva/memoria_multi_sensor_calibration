0f11e44592b1593d740be0b4d5209d41bad1ce4a
Lucas Silva <lucas_ss98@hotmail.com>
2025-12-04 16:45:59 -0300

    fix: arregla sincronizador de stereo.

--- stereo_detector/src/stereo_to_pointcloud.cpp
+++ stereo_detector/src/stereo_to_pointcloud.cpp
@@ -2,6 +2,7 @@
 #include <image_transport/image_transport.h>
 #include <image_transport/subscriber_filter.h>
 #include <sensor_msgs/Image.h>
+#include <stereo_msgs/DisparityImage.h>
 #include <sensor_msgs/CameraInfo.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <message_filters/subscriber.h>
@@ -18,11 +19,12 @@
 class StereoProjector {
 public:
     StereoProjector(ros::NodeHandle& nh)
-        : it_(nh),
+        : nh_(nh),
+          it_(nh),
           left_image_sub_(it_, "/zed/zed_node/left/image_rect_color", 1),
-          disparity_sub_(it_, "/zed/zed_node/disparity/disparity_image", 1),
-          left_info_sub_(nh, "/zed/zed_node/left/camera_info", 1),
-          right_info_sub_(nh, "/zed/zed_node/right/camera_info", 1),
+          disparity_sub_(nh_, "/zed/zed_node/disparity/disparity_image", 1),
+          left_info_sub_(nh_, "/zed/zed_node/left/camera_info", 1),
+          right_info_sub_(nh_, "/zed/zed_node/right/camera_info", 1),
           sync_(SyncPolicy(10), left_image_sub_, disparity_sub_, left_info_sub_, right_info_sub_)
     {
         sync_.registerCallback(boost::bind(&StereoProjector::callback, this, _1, _2, _3, _4));
@@ -34,26 +36,30 @@ public:
 private:
     typedef message_filters::sync_policies::ApproximateTime<
         sensor_msgs::Image,
-        sensor_msgs::Image,
+        stereo_msgs::DisparityImage,
         sensor_msgs::CameraInfo,
         sensor_msgs::CameraInfo> SyncPolicy;
 
+    ros::NodeHandle nh_;
     image_transport::ImageTransport it_;
-    image_transport::SubscriberFilter left_image_sub_, disparity_sub_;
+    image_transport::SubscriberFilter left_image_sub_;
+    
+    message_filters::Subscriber<stereo_msgs::DisparityImage> disparity_sub_;
     message_filters::Subscriber<sensor_msgs::CameraInfo> left_info_sub_, right_info_sub_;
     message_filters::Synchronizer<SyncPolicy> sync_;
     ros::Publisher pub_cloud_;
 
     void callback(const sensor_msgs::ImageConstPtr& left_msg,
-                  const sensor_msgs::ImageConstPtr& disp_msg,
+                  const stereo_msgs::DisparityImageConstPtr& disp_msg,
                   const sensor_msgs::CameraInfoConstPtr& left_info,
                   const sensor_msgs::CameraInfoConstPtr& right_info)
     {
+        ROS_INFO("Ejecutando callback...");
+
         // Convert images
-        cv_bridge::CvImageConstPtr left_cv, disp_cv;
+        cv_bridge::CvImageConstPtr left_cv;
         try {
             left_cv = cv_bridge::toCvShare(left_msg, "bgr8");
-            disp_cv = cv_bridge::toCvShare(disp_msg, "32FC1");
         }
         catch (cv_bridge::Exception& e) {
             ROS_ERROR("Error en cv_bridge: %s", e.what());
@@ -64,31 +70,32 @@ private:
         image_geometry::StereoCameraModel model;
         model.fromCameraInfo(*left_info, *right_info);
 
-        int width = disp_cv->image.cols;
-        int height = disp_cv->image.rows;
+        int width = disp_msg->image.width;
+        int height = disp_msg->image.height;
+
+        ROS_INFO("Ancho: %d; alto: %d ", width, height);
 
         pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>());
         cloud->header.frame_id = left_info->header.frame_id;
-        cloud->width = width * height;
-        cloud->height = 1;
+        cloud->width = width;
+        cloud->height = height;
         cloud->is_dense = false;
-        cloud->points.resize(cloud->width);
+        cloud->points.resize(width * height);
 
         int idx = 0;
 
         for (int v = 0; v < height; v++) {
             for (int u = 0; u < width; u++) {
+                float disparity_value = disp_msg->image.data[v*width+u];
 
-                float disp = disp_cv->image.at<float>(v, u);
-
-                if (std::isnan(disp) || disp <= 0.1) {
+                if (std::isnan(disparity_value) || disparity_value <= disp_msg->min_disparity) {
                     cloud->points[idx].x = cloud->points[idx].y = cloud->points[idx].z = NAN;
                     idx++;
                     continue;
                 }
 
                 cv::Point3d pt;
-                model.projectDisparityTo3d(cv::Point2d(u, v), disp, pt);
+                model.projectDisparityTo3d(cv::Point2d(u, v), disparity_value, pt);
 
                 cloud->points[idx].x = pt.x;
                 cloud->points[idx].y = pt.y;
